8f53d38abeea78d92800a982818ad955fa0edef4
Paul Coroneos <paul@pcoroneos.com>
2021-01-31 00:39:44 -0600

    seed initial data

---
+++ constants/data.ts
@@ -0,0 +1,105 @@
+const data = new Map<string, Record<string, any>>([
+  // hat data
+  [
+    "beanie",
+    {
+      name: "beanie",
+      desc: "A fleece lined beanie will keep your head warm on a cold day!",
+    },
+  ],
+  [
+    "cap",
+    {
+      name: "cap",
+      desc:
+        "A cap with a lip will protect your face from sunny rays and rainy days. For hotter days consider wearing a visor.",
+    },
+  ],
+  //general data
+  [
+    "sunglasses",
+    {
+      name: "sunglasses",
+      desc:
+        "A good pair of sunglasses will protect your eyes from UV radiation during the day.",
+    },
+  ],
+  [
+    "identification",
+    {
+      name: "identification",
+      desc:
+        "In the event of an emergency it's always good to keep some identification you.",
+    },
+  ],
+  //bottom data
+  [
+    "shorts",
+    {
+      name: "running shorts",
+      desc:
+        "A nice lightweight pair of shorts are your best friend. Pick a length that suites your comfort.",
+    },
+  ],
+  [
+    "tights",
+    {
+      name: "running tights",
+      desc:
+        "For cold breezy runs a pair of running tights will keep your legs nice and warm.",
+    },
+  ],
+  //top data
+  [
+    "shirt",
+    {
+      name: "shirt",
+      desc:
+        "Choose a top with good moisture wicking material to stay dry and cool",
+    },
+  ],
+  [
+    "longSleeveShirt",
+    {
+      name: "long sleeve shirt",
+      desc: "Works similar to a shirt but with sleeves to keep your arms warm.",
+    },
+  ],
+  //glove data
+  [
+    "gloves",
+    {
+      name: "gloves",
+      desc:
+        "A thin pair of gloves with our without technical fleece can help to keep your hands warm on cool days.",
+    },
+  ],
+  [
+    "heavyGloves",
+    {
+      name: "heavy gloves",
+      desc:
+        "A solid pair of mittens or even adding a pair of gloves liners in colder weather can help keep your fingers nimble.",
+    },
+  ],
+  //jacket data
+  [
+    "lightJacket",
+    {
+      name: "light jacket",
+      desc: "A light windbreaker, thin jacket, or pullover is good for blustery days.",
+    },
+  ],[
+    "rainJacket",
+    {
+      name: "rain jacket",
+      desc: "A nice rain jacket is helpful on wetter days. Look for goretex or something with good water repellent capability",
+    }
+  ],
+  ["winterJacket",{
+    name:"winter jacket",
+    desc:"For very cold days a thick jacket that protects you from the wind. You may consider adding additional layers instead to stay cool as you warm up."
+  }]
+]);
+
+export default data;
